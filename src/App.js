46bf7a15ca4c1ad4aaec6056f23b71b4a0b38bda
padrisimo <nopuedoparardemolar@outlook.com>
2018-01-22 23:58:59 +0100

    initial apolo setup

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import React, { Component } from 'react';
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient, InMemoryCache } from 'apollo-client-preset';
+import { createHttpLink } from 'apollo-link-http';
 import './App.css';
 import Contacts from './Contacts';
+
+const client = new ApolloClient({
+  link: createHttpLink(),
+  cache: new InMemoryCache()
+});
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h2>CRM</h2>
-        </header>
-        <Contacts/>
-      </div>
+      <ApolloProvider client={client}>
+        <div className="App">
+          <header className="App-header">
+            <h2>CRM</h2>
+          </header>
+          <Contacts />
+        </div>
+      </ApolloProvider>
     );
   }
 }
--- src/Contacts.js
+++ src/Contacts.js
@@ -1,10 +1,31 @@
 import React from 'react';
+import { graphql } from 'react-apollo';
+import gql from 'graphql-tag';
 
-export default () => {
+const Contacts = ({ data: { loading, error, contacts } }) => {
+  if (loading) {
+    return <p>Loading...</p>
+  }
+  if (error) {
+    return <p>{error.message}</p>
+  }
   return (
     <ul>
-      <li>Peter Parker</li>
-      <li>Frank Castle</li>
+      {contacts.map(item => (
+        <li key={item.id}>{item.firstName} {item.lastName}</li>
+      ))}
     </ul>
   )
 };
+
+export const contactsListQuery = gql`
+  query ContactsQuery {
+    contacts {
+      id
+      firstName
+      lastName
+    }
+  }
+`;
+
+export default graphql(contactsListQuery)(Contacts);
