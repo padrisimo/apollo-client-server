ccdd33a90553091aa6c0a20e0a12c1be6e5bcc89
padrisimo <nopuedoparardemolar@outlook.com>
2018-01-26 14:12:51 +0100

    intro to subscriptions and schema

--- server/server.js
+++ server/server.js
@@ -2,17 +2,35 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import { graphiqlExpress, graphqlExpress } from 'apollo-server-express';
 import cors from 'cors';
+import { execute, subscribe } from 'graphql';
+import { createServer } from 'http';
+import { SubscriptionServer } from 'subscriptions-transport-ws';
 import { schema } from './src/schema';
 
 const myGraphQLSchema = schema;
 const PORT = 4000;
 const server = express();
 
-server.use('*', cors({origin: 'http://localhost:3000'}));
+server.use('*', cors({ origin: 'http://localhost:3000' }));
 
 server.use('/graphql', bodyParser.json(), graphqlExpress({ schema: myGraphQLSchema }));
-server.use('/graphiql', bodyParser.json(), graphiqlExpress({ endpointURL: '/graphql' }));
+server.use('/graphiql', bodyParser.json(), graphiqlExpress({
+    endpointURL: '/graphql',
+    subscriptionsEndpoint: `ws://localhost:${PORT}/subscriptions`
+}));
 
-server.listen(PORT, () => 
-    console.log(`GraphQL server is runing on port ${PORT}`)
+const ws = createServer(server);
+
+ws.listen(PORT, () => {
+    console.log(`GraphQL server is runing on port ${PORT}`);
+
+    new  SubscriptionServer({
+        execute,
+        subscribe,
+        schema
+    }, {
+        server: ws,
+        path: '/subscriptions'
+    })
+}
 );
