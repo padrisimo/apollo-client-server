8c1656cebef1c3cdf1b6c2bdd66c220a66a2230a
padrisimo <adrianfernandezchamizo@gmail.com>
2018-02-08 16:50:35 +0100

    create a custom resolver on client

--- client/src/core/App.js
+++ client/src/core/App.js
@@ -13,7 +13,7 @@ import ContactSingle from './ContactSingle';
 
 const PORT = 4000;
 
-const networkInterface =  new HttpLink({ uri: `http://localhost:${PORT}/graphql` });
+const networkInterface = new HttpLink({ uri: `http://localhost:${PORT}/graphql` });
 
 const wsClient = new SubscriptionClient(`ws://localhost:${PORT}/subscriptions`, {
   reconnect: true
@@ -21,26 +21,51 @@ const wsClient = new SubscriptionClient(`ws://localhost:${PORT}/subscriptions`, 
 
 const networkInterfaceWithSubscriptions = addGraphQLSubscriptions(
   networkInterface,
-  client
-)
+  wsClient
+);
+
+const dataIdFromObject = (result) => {
+  if (result.__typename) {
+    if (result.id !== undefined) {
+      return `${result.__typename}:${result.id}`
+    }
+  }
+  return null;
+};
 
 const client = new ApolloClient({
   link: networkInterfaceWithSubscriptions,
-  cache: new InMemoryCache()
+  cache: new InMemoryCache(),
+  customResolvers: {
+    Query: {
+      contact: (__, args) => {
+        return toIdValue(dataIdFromObject({ __typename: 'Contact', id: args['id'] }))
+      }
+    }
+  },
+  dataIdFromObject
 });
 
 class App extends Component {
   render() {
     return (
-      <ApolloProvider client={client}>
-        <div className="App">
-          <header className="App-header">
-            <h2>CRM</h2>
-          </header>
-          <AddContact />
-          <Contacts />
-        </div>
-      </ApolloProvider>
+      <BrowserRouter>
+        <ApolloProvider client={client}>
+          <div className="App">
+            <header className="App-header">
+              <Link to="/">
+                <h2>CRM</h2>
+              </Link>
+            </header>
+            <AddContact />
+            <Switch>
+              <Route path='/' component={Contacts}/>
+              <Route path='/contact/:contactId' component={ContactSingle}/>
+            </Switch>
+            <Contacts />
+          </div>
+        </ApolloProvider>
+      </BrowserRouter>
     );
   }
 }
